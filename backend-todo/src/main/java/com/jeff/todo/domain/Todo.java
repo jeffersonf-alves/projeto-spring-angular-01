1e53e3c06f194d56479e65821b720300ec288acd
Jefferson Alves <jefff9910@outlook.com>
2022-10-15 23:26:10 -0300

    Projeto criado

---
+++ backend-todo/src/main/java/com/jeff/todo/domain/Todo.java
@@ -0,0 +1,29 @@
+package com.jeff.todo.domain;
+
+import java.time.LocalDateTime;
+
+public class Todo {
+	
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataParaFinalizar;
+	private Boolean finalizado = false;
+	
+	public Todo() {
+		super();
+	}
+
+	public Todo(Integer id, String titulo, String descricao, LocalDateTime dataParaFinalizar, Boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFinalizar = dataParaFinalizar;
+		this.finalizado = finalizado;
+	}
+	
+	
+	
+	
+}
