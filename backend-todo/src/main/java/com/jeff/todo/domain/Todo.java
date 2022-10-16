8d3af24030551a510761d45c5db8f32771d73999
Jefferson Alves <jefff9910@outlook.com>
2022-10-16 09:48:25 -0300

    Classe domain criada

--- backend-todo/src/main/java/com/jeff/todo/domain/Todo.java
+++ backend-todo/src/main/java/com/jeff/todo/domain/Todo.java
@@ -1,10 +1,25 @@
 package com.jeff.todo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class Todo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String titulo;
 	private String descricao;
 	private LocalDateTime dataParaFinalizar;
@@ -21,6 +36,63 @@ public class Todo {
 		this.descricao = descricao;
 		this.dataParaFinalizar = dataParaFinalizar;
 		this.finalizado = finalizado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataParaFinalizar() {
+		return dataParaFinalizar;
+	}
+
+	public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+		this.dataParaFinalizar = dataParaFinalizar;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
